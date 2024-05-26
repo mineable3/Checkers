e8b6b6748d316c40c75fbd315991f1dcd80368b5
Emmet Spaeth <emmet.spaeth@gmail.com>
2024-05-26 10:37:30 -0400

    creating custom move exception

---
+++ IllegalMoveException.java
@@ -0,0 +1,16 @@
+public class IllegalMoveException extends Exception {
+
+  public IllegalMoveException() {}
+
+  public IllegalMoveException(String message) {
+    super(message);
+  }
+
+  public IllegalMoveException(Throwable cause) {
+    super(cause);
+  }
+
+  public IllegalMoveException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}
