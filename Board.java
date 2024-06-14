644e52b8c0af2aba32f589f6efd0ca5addda6af0
Emmet Spaeth <emmet.spaeth@gmail.com>
2024-06-14 13:16:12 -0400

    can't remember + .gitignore

--- Board.java
+++ Board.java
@@ -6,174 +6,191 @@ public class Board {
     pieces = new Checker[24];
 
     //Top pieces (black)
-    pieces[0] = new Checker(false, 2, 1);
-    pieces[1] = new Checker(false, 4, 1);
-    pieces[2] = new Checker(false, 6, 1);
-    pieces[3] = new Checker(false, 8, 1);
-    pieces[4] = new Checker(false, 1, 2);
-    pieces[5] = new Checker(false, 3, 2);
-    pieces[6] = new Checker(false, 5, 2);
-    pieces[7] = new Checker(false, 7, 2);
-    pieces[8] = new Checker(false, 2, 3);
-    pieces[9] = new Checker(false, 4, 3);
-    pieces[10] = new Checker(false, 6, 3);
-    pieces[11] = new Checker(false, 8, 3);
+    pieces[0] = new Checker(false, 1);
+    pieces[1] = new Checker(false, 2);
+    pieces[2] = new Checker(false, 3);
+    pieces[3] = new Checker(false, 4);
+    pieces[4] = new Checker(false, 5);
+    pieces[5] = new Checker(false, 6);
+    pieces[6] = new Checker(false, 7);
+    pieces[7] = new Checker(false, 8);
+    pieces[8] = new Checker(false, 9);
+    pieces[9] = new Checker(false, 10);
+    pieces[10] = new Checker(false, 11);
+    pieces[11] = new Checker(false, 12);
 
     //Bottom pieces (red)
-    pieces[12] = new Checker(true, 1, 6);
-    pieces[13] = new Checker(true, 3, 6);
-    pieces[14] = new Checker(true, 5, 6);
-    pieces[15] = new Checker(true, 7, 6);
-    pieces[16] = new Checker(true, 2, 7);
-    pieces[17] = new Checker(true, 4, 7);
-    pieces[18] = new Checker(true, 6, 7);
-    pieces[19] = new Checker(true, 8, 7);
-    pieces[20] = new Checker(true, 1, 8);
-    pieces[21] = new Checker(true, 3, 8);
-    pieces[22] = new Checker(true, 5, 8);
-    pieces[23] = new Checker(true, 7, 8);
+    pieces[12] = new Checker(true, 21);
+    pieces[13] = new Checker(true, 22);
+    pieces[14] = new Checker(true, 23);
+    pieces[15] = new Checker(true, 24);
+    pieces[16] = new Checker(true, 25);
+    pieces[17] = new Checker(true, 26);
+    pieces[18] = new Checker(true, 27);
+    pieces[19] = new Checker(true, 28);
+    pieces[20] = new Checker(true, 29);
+    pieces[21] = new Checker(true, 30);
+    pieces[22] = new Checker(true, 31);
+    pieces[23] = new Checker(true, 32);
   }
 
   private void capturePiece(Checker c) {
     c.setAlive(false);
-    c.setCoords(-1, -1);
+    c.setLocation(-1);
   }
 
-  public void moveRedPiece(int pieceX, int pieceY, int x, int y) throws IllegalMoveException {
-    if(findPiece(x, y) != null) {
-      throw new IllegalMoveException("A piece is in the way of that move");
+  public void movePiece(int start, int end) {
+    if(checkMoveIsLegal(start, end)) {
+      if(Math.abs(start - end) > 6) {
+        // capturePiece(something);
+      }
+      findPiece(start).setLocation(end);
+    } else {
+      System.out.println("That is not a legal move");
     }
-
-    if(findPiece(pieceX, pieceY) == null) {
-      throw new IllegalMoveException("There is no piece to move");
-    }
-
-    if(x < 1 || x > 8 || y < 1 || y > 8) {
-      throw new IllegalMoveException("Move is off the board");
-    }
-
-    // TODO add color based logic on capturing
-    // Capturing a piece
-    if(pieceX - x == 2 && pieceY - y == 2 && findPiece(pieceX - 1, pieceY - 1) != null) {
-      findPiece(pieceX, pieceY).setCoords(x, y);
-      capturePiece(findPiece(pieceX - 1, pieceY - 1));
-      return;
-    }
-
-    if(pieceX - x == -2 && pieceY - y == 2 && findPiece(pieceX + 1, pieceY - 1) != null) {
-      findPiece(pieceX, pieceY).setCoords(x, y);
-      capturePiece(findPiece(pieceX + 1, pieceY - 1));
-      return;
-    }
-
-    if(findPiece(pieceX, pieceY).isKing()
-        && pieceX - x == -2
-        && pieceY - y == -2
-        && findPiece(pieceX + 1, pieceY + 1) != null) {
-      findPiece(pieceX, pieceY).setCoords(x, y);
-      capturePiece(findPiece(pieceX + 1, pieceY + 1));
-      return;
-    }
-
-    if(findPiece(pieceX, pieceY).isKing()
-        && pieceX - x == 2
-        && pieceY - y == -2
-        && findPiece(pieceX - 1, pieceY + 1) != null) {
-      findPiece(pieceX, pieceY).setCoords(x, y);
-      capturePiece(findPiece(pieceX - 1, pieceY + 1));
-      return;
-    }
-
-    if(x != pieceX + 1 && x != pieceX - 1) {
-      throw new IllegalMoveException();
-    }
-
-    if(y != pieceY - 1 && y != pieceY + 1) {
-      throw new IllegalMoveException("Pieces can only move one space at a time");
-    }
-
-    if (y != pieceY - 1 && !findPiece(pieceX, pieceY).isKing()) {
-      throw new IllegalMoveException("Not kinged pieces can only move forward by one space");
-    }
-
-    findPiece(pieceX, pieceY).setCoords(x, y);
-  }
-
-  public void moveBlackPiece(int pieceX, int pieceY, int x, int y) throws IllegalMoveException {
-    if(findPiece(x, y) != null) {
-      throw new IllegalMoveException("A piece is in the way of that move");
-    }
-
-    if(findPiece(pieceX, pieceY) == null) {
-      throw new IllegalMoveException("There is no piece to move");
-    }
-
-    if(x < 1 || x > 8 || y < 1 || y > 8) {
-      throw new IllegalMoveException("Move is off the board");
-    }
-
-    // Capturing a piece
-    if(pieceX - x == 2 && pieceY - y == -2 && findPiece(pieceX - 1, pieceY + 1) != null) {
-      findPiece(pieceX, pieceY).setCoords(x, y);
-      capturePiece(findPiece(pieceX - 1, pieceY + 1));
-      return;
-    }
-
-    if(pieceX - x == -2 && pieceY - y == -2 && findPiece(pieceX + 1, pieceY + 1) != null) {
-      findPiece(pieceX, pieceY).setCoords(x, y);
-      capturePiece(findPiece(pieceX + 1, pieceY + 1));
-      return;
-    }
-
-    if(findPiece(pieceX, pieceY).isKing()
-        && pieceX - x == -2
-        && pieceY - y == 2
-        && findPiece(pieceX + 1, pieceY - 1) != null) {
-      findPiece(pieceX, pieceY).setCoords(x, y);
-      capturePiece(findPiece(pieceX + 1, pieceY - 1));
-      return;
-    }
-
-    if(findPiece(pieceX, pieceY).isKing()
-        && pieceX - x == 2
-        && pieceY - y == 2
-        && findPiece(pieceX - 1, pieceY - 1) != null) {
-      findPiece(pieceX, pieceY).setCoords(x, y);
-      capturePiece(findPiece(pieceX - 1, pieceY - 1));
-      return;
-    }
-
-    if(x != pieceX + 1 && x != pieceX - 1) {
-      throw new IllegalMoveException();
-    }
-
-    if(y != pieceY - 1 && y != pieceY + 1) {
-      throw new IllegalMoveException("Pieces can only move one space at a time");
-    }
-
-    if (y != pieceY + 1 && !findPiece(pieceX, pieceY).isKing()) {
-      throw new IllegalMoveException("Not kinged pieces can only move forward by one space");
-    }
-
-    findPiece(pieceX, pieceY).setCoords(x, y);
   }
 
   public void kingEligiblePieces() {
     for(Checker c : pieces) {
-      if(c.isRed() && c.getY() == 1) {
+      if(c.isRed() && (c.getLocation() == 1 || c.getLocation() == 2 || c.getLocation() == 3 || c.getLocation() == 4)) {
         c.setKinged(true);
-      } else if (!c.isRed() && c.getY() == 8) {
+      } else if (!c.isRed() && (c.getLocation() == 29 || c.getLocation() == 30 || c.getLocation() == 31 || c.getLocation() == 32)) {
         c.setKinged(true);
       }
     }
   }
 
-  public Checker findPiece(int x, int y) {
+  public Checker findPiece(int location) {
     for(Checker c : pieces) {
-      if(c.getX() == x && c.getY() == y) {
+      if(c.getLocation() == location) {
         return c;
       }
     }
     return null;
+  }
+
+  private boolean checkRedLegalMove(boolean kinged, int start, int end) {
+    if(start == 1 ||
+        start == 2 ||
+        start == 3 ||
+        start == 4 ||
+        start == 9 ||
+        start == 10 ||
+        start == 11 ||
+        start == 12 ||
+        start == 17 ||
+        start == 18 ||
+        start == 19 ||
+        start == 20 ||
+        start == 25 ||
+        start == 26 ||
+        start == 27 ||
+        start == 28) {
+      if(end == start - 4 || end == start - 3) {
+        return true;
+      } else if(kinged && (end == start + 4 || end == start + 5)) {
+        return true;
+      } else if((end == start - 9 && findPiece(start - 4) != null) || (end == start - 7 && findPiece(start - 3) != null)) {
+        return true;
+      } else if(kinged && (end == start + 7 && findPiece(start + 4) != null) || (end == start + 9 && findPiece(start + 5) != null)) {
+        return true;
+      }
+    }
+
+    if(start == 5 ||
+        start == 6 ||
+        start == 7 ||
+        start == 8 ||
+        start == 13 ||
+        start == 14 ||
+        start == 15 ||
+        start == 16 ||
+        start == 21 ||
+        start == 22 ||
+        start == 23 ||
+        start == 24 ||
+        start == 29 ||
+        start == 30 ||
+        start == 31 ||
+        start == 32) {
+      if(end == start - 4 || end == start - 5) {
+        return true;
+      } else if(kinged && (end == start + 4 || end == start + 3)) {
+        return true;
+      } else if((end == start - 9 && findPiece(start - 5) != null) || (end == start - 7 && findPiece(start - 4) != null)) {
+        return true;
+      } else if(kinged && (end == start + 7 && findPiece(start + 3) != null) || (end == start + 9 && findPiece(start + 4) != null)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private boolean checkBlackLegalMove(boolean kinged, int start, int end) {
+    if(start == 1 ||
+        start == 2 ||
+        start == 3 ||
+        start == 4 ||
+        start == 9 ||
+        start == 10 ||
+        start == 11 ||
+        start == 12 ||
+        start == 17 ||
+        start == 18 ||
+        start == 19 ||
+        start == 20 ||
+        start == 25 ||
+        start == 26 ||
+        start == 27 ||
+        start == 28) {
+      if(end == start + 4 || end == start + 5) {
+        return true;
+      } else if(kinged && (end == start - 4 || end == start - 3)) {
+        return true;
+      }
+    }
+
+    if(start == 5 ||
+        start == 6 ||
+        start == 7 ||
+        start == 8 ||
+        start == 13 ||
+        start == 14 ||
+        start == 15 ||
+        start == 16 ||
+        start == 21 ||
+        start == 22 ||
+        start == 23 ||
+        start == 24 ||
+        start == 29 ||
+        start == 30 ||
+        start == 31 ||
+        start == 32) {
+      if(end == start + 4 || end == start + 3) {
+        return true;
+      } else if(kinged && (end == start - 4 || end == start - 5)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  public boolean checkMoveIsLegal(int start, int end) {
+    Checker source = findPiece(start);
+
+    if(findPiece(end) != null || findPiece(start) == null) {
+      return false;
+    }
+
+    if(start < 1 || start > 32 || end < 1 || end > 32 || start == end) {
+      return false;
+    }
+
+    if(source.isRed()) {
+      return checkRedLegalMove(source.isKing(), start, end);
+    } else {
+      return checkBlackLegalMove(source.isKing(), start, end);
+    }
   }
 }
--- Game.java
+++ Game.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Game {
 
-  private Board board;
+  public Board board;
   private Scanner scanner;
   private StringBuilder display;
   private boolean playing;
@@ -49,29 +49,28 @@ public class Game {
       displayGame();
       playerMove();
       board.kingEligiblePieces();
-      displayGame();
-      botMove();
-      board.kingEligiblePieces();
     }
   }
 
   private void displayGame() {
     String letters = new String("abcdefgh");
+
     display.delete(0, display.length());
     display.append(CLEAR_SCREEN);
     display.append("   1  2  3  4  5  6  7  8\n");
     display.append("  ------------------------\n");
+
     for(int i = 0; i < 8; i++) {
       display.append(letters.charAt(i) + "|");
       for (int j = i; j < 8 + i; j++) {
-        if(board.findPiece(j - i + 1, i+1) == null) {
+        if(board.findPiece(locationFromDisplay(i, j)) == null) {
           if(j % 2 == 0) {
             display.append(ANSI_WHITE_BACKGROUND + "   " + ANSI_RESET);
           } else {
             display.append(ANSI_BLACK_BACKGROUND + "   " + ANSI_RESET);
           }
-        } else if (board.findPiece(j-i+1, i+1).isRed()) {
-          if(board.findPiece(j-i+1, i+1).isKing()) {
+        } else if(board.findPiece(locationFromDisplay(i, j)).isRed()) {
+          if(board.findPiece(locationFromDisplay(i, j)).isKing()) {
             if(j % 2 == 0) {
               display.append(ANSI_WHITE_BACKGROUND + ANSI_RED + " 0 " + ANSI_RESET);
             } else {
@@ -85,7 +84,7 @@ public class Game {
             }
           }
         } else {
-          if(board.findPiece(j-i+1, i+1).isKing()) {
+          if(board.findPiece(locationFromDisplay(i, j)).isKing()) {
             if(j % 2 == 0) {
               display.append(ANSI_WHITE_BACKGROUND + ANSI_BLACK + " 0 " + ANSI_RESET);
             } else {
@@ -99,12 +98,83 @@ public class Game {
             }
           }
         }
-
       }
       display.append("\n");
     }
     display.append("  ------------------------");
     System.out.println(display.toString());
+  }
+
+  private int locationFromDisplay(int i, int j) {
+    String num = String.valueOf(i) + String.valueOf(j);
+
+    switch(num) {
+      case "01":
+        return 1;
+      case "03":
+        return 2;
+      case "05":
+        return 3;
+      case "07":
+        return 4;
+      case "11":
+        return 5;
+      case "13":
+        return 6;
+      case "15":
+        return 7;
+      case "17":
+        return 8;
+      case "23":
+        return 9;
+      case "25":
+        return 10;
+      case "27":
+        return 11;
+      case "29":
+        return 12;
+      case "33":
+        return 13;
+      case "35":
+        return 14;
+      case "37":
+        return 15;
+      case "39":
+        return 16;
+      case "45":
+        return 17;
+      case "47":
+        return 18;
+      case "49":
+        return 19;
+      case "411":
+        return 20;
+      case "55":
+        return 21;
+      case "57":
+        return 22;
+      case "59":
+        return 23;
+      case "511":
+        return 24;
+      case "67":
+        return 25;
+      case "69":
+        return 26;
+      case "611":
+        return 27;
+      case "613":
+        return 28;
+      case "77":
+        return 29;
+      case "79":
+        return 30;
+      case "711":
+        return 31;
+      case "713":
+        return 32;
+    }
+    return -1;
   }
 
   private void playerMove() {
@@ -180,92 +250,7 @@ public class Game {
         y = -1;
     }
 
-    try {
-      board.moveRedPiece(pieceX, pieceY, x, y);
-    } catch (IllegalMoveException e) {
-      e.printStackTrace();
-    }
-  }
-
-  private void botMove() {
-
-    System.out.println("Make your move: ");
-    String move = scanner.nextLine();
-
-    if(move.equals("exit")) {
-      playing = false;
-      return;
-    }
-
-    int pieceX = Integer.parseInt(move.substring(1, 2));
-    int pieceY;
-
-    switch(move.substring(0, 1)) {
-      case "a":
-        pieceY = 1;
-        break;
-      case "b":
-        pieceY = 2;
-        break;
-      case "c":
-        pieceY = 3;
-        break;
-      case "d":
-        pieceY = 4;
-        break;
-      case "e":
-        pieceY = 5;
-        break;
-      case "f":
-        pieceY = 6;
-        break;
-      case "g":
-        pieceY = 7;
-        break;
-      case "h":
-        pieceY = 8;
-        break;
-      default:
-        pieceY = -1;
-    }
-
-    int x = Integer.parseInt(move.substring(4, 5));
-    int y;
-
-    switch(move.substring(3, 4)) {
-      case "a":
-        y = 1;
-        break;
-      case "b":
-        y = 2;
-        break;
-      case "c":
-        y = 3;
-        break;
-      case "d":
-        y = 4;
-        break;
-      case "e":
-        y = 5;
-        break;
-      case "f":
-        y = 6;
-        break;
-      case "g":
-        y = 7;
-        break;
-      case "h":
-        y = 8;
-        break;
-      default:
-        y = -1;
-    }
-
-    try {
-      board.moveBlackPiece(pieceX, pieceY, x, y);
-    } catch (IllegalMoveException e) {
-      e.printStackTrace();
-    }
+    board.movePiece((pieceY-1)*4+((pieceX+1)/2), (y-1)*4+((x+1)/2));
   }
 
   private void displayMenu() {}
